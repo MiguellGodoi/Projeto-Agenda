950c29ccaf92a71ff8dbaff66f7bda3091eefb50
MiguelGodoi <dellmazza@gmail.com>
2026-09-10 21:33:02 -0300

    Identação

--- projetoagenda/frontend/modules/Login.js
+++ projetoagenda/frontend/modules/Login.js
@@ -1,6 +1,6 @@
 import validator from 'validator';
 
-export default class Login {
+export default class Login  {
   constructor(formClass) {
     this.form = document.querySelector(formClass);
   }
